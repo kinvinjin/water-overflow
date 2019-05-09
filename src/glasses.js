11527094517ec09263b8e163acb64165ad6c9858
Kinvin Jin <kinvinjin@gmail.com>
2019-05-09 15:50:18 +1000

    use dp algorithm to calculate the result

--- src/glasses.js
+++ src/glasses.js
@@ -4,11 +4,49 @@ class Glasses {
     constructor(k) {
         this.n = this.getInputNum(k);
     }
+    /**
+     * Calculate how many glasses of water for the input water:
+     * k (L) * 1000 / 250 ml
+     * 
+     * @param {Number} k water (L)
+     * @return {Number} how many glasses of water.
+     */
     getInputNum(k) {
         return k * 1000 / VOLUME;
     }
+    /**
+     * Calculate the amount of water in the glass with sepcified row and column number
+     * 
+     * @param {Number} k water (L)
+     * @return {Number} the amount of water (ml).
+     */
     getWater(i, j) {
-        return 0;
+        let result = this.calculate(i, j, this.n);
+        return result * VOLUME;
+    }
+    /**
+     * Use dynamic programming to calculate the result 
+     * 
+     * @param {Number} r row number
+     * @param {Number} c column number
+     * @param {Number} n the number of glass water as input
+     * @return {Number} water in the glass.
+     */
+    calculate(r, c, n) {
+        const rows = r + 2;
+        const d = [...Array(rows)].map(()=>Array(rows).fill(0));
+        
+        d[0][0] = n;
+        for (let i=0; i<=r; i++) {
+            for (let j=0; j<=i; j++) {
+                if (d[i][j] > 1) {
+                    d[i+1][j] += (d[i][j] - 1) / 2;
+                    d[i+1][j+1] += (d[i][j] - 1) / 2;
+                }
+            }
+        }
+        
+        return d[r][c] > 1? 1 : d[r][c];
     }
 }
 
--- test/glasses.spec.js
+++ test/glasses.spec.js
@@ -4,7 +4,37 @@ const Glasses = require('../src/glasses');
 const testdata = {
     input: 1, // 1L
     output: 4, // 4 glasses of water
-    glasses: [[0]]
+    glasses: [{
+      i: 0,
+      j: 0,
+      result: 1,
+      water: 250
+    }, {
+      i: 1,
+      j: 0,
+      result: 1,
+      water: 250
+    }, {
+      i: 1,
+      j: 1,
+      result: 1,
+      water: 250
+    }, {
+      i: 2,
+      j: 0,
+      result: 0.25,
+      water: 62.5
+    }, {
+      i: 2,
+      j: 1,
+      result: 0.5,
+      water: 125
+    }, {
+      i: 2,
+      j: 2,
+      result: 0.25,
+      water: 62.5
+    }]
 };
 describe('glasses.js', function() {
     describe('getInputNum(k)', function() {
@@ -14,12 +44,20 @@ describe('glasses.js', function() {
             expect(glasses.n).to.equal(testdata.output);
         });
     });
-    describe('getWater(i, j)', function() {
-        it('should return the amount of water for the specified glass', function() {
+    describe('calculate(r, c, n)', function() {
+        it('should return the number of glass water for the glass with specific row and column', function() {
             const glasses = new Glasses(testdata.input);
             expect(glasses.getInputNum(testdata.input)).to.equal(testdata.output);
-            let i = 0, j = 0;
-            expect(glasses.getWater(i, j)).to.equal(testdata.glasses[i][j]);
+            testdata.glasses.forEach((item)=> {
+                expect(glasses.calculate(item.i, item.j, testdata.output)).to.equal(item.result);
+            });
+        });
+    });
+    describe('getWater(i, j)', function() {
+        it('should return the amount of water (ml) for the specified glass', function() {
+            const glasses = new Glasses(testdata.input);
+            expect(glasses.getInputNum(testdata.input)).to.equal(testdata.output);
+            expect(glasses.getWater(testdata.glasses[0].i, testdata.glasses[0].j)).to.equal(testdata.glasses[0].water);
         });
     });
 });
